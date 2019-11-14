b04eeb3f5b90b6e02a2c11d434afe502c7d11531
agoetc <b6151@oic.jp>
2019-11-14 15:40:53 +0900

    リレーションをもつtableを追加

--- todo_list/admin.py
+++ todo_list/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 
-from .models import Todo
+from .models import Todo, Customer, Profile
+
 
 @admin.register(Todo)
 class Todo(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Customer)
+class Customer(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Profile)
+class Profile(admin.ModelAdmin):
     pass
--- todo_list/models.py
+++ todo_list/models.py
@@ -1,4 +1,33 @@
 from django.db import models
+import uuid as uuid_lib
+
 
 class Todo(models.Model):
     todo = models.CharField(max_length=256)
+
+
+class Profile(models.Model):
+    profile_id = models.UUIDField(
+        db_index=True,
+        default=uuid_lib.uuid4,
+        editable=False
+    )
+
+    email_address = models.EmailField(max_length=254, unique=False)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+
+
+class Customer(models.Model):
+    customer_id = models.UUIDField(
+        db_index=True,
+        default=uuid_lib.uuid4,
+        editable=False
+    )
+
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
